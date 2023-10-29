0d6c3e25865c12a3f8ec659f372d6890b268a73b
ashishasvi <90001547+ashishasvi@users.noreply.github.com>
2023-10-29 22:38:24 +0530

    initialise

---
+++ group.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+
+
+def process_excel(input_file,output_file,sumcoloumn):
+    df=pd.DataFrame(openpyxl.load_workbook(input_file).active.values)
+    df.columns=df.iloc[0]
+    df=df[1:]
+
+    
+    oldcol=list(df.columns)
+    gplist=[]
+
+     #input less than one coloumn 
+    popcol=oldcol.pop(sumcoloumn-1)
+    oldcol.append(popcol)
+    df.columns= range(1,len(df.columns)+1)
+
+
+    for i in range(1,len(df.columns)+1):
+        if i==sumcoloumn:
+            pass
+        else:
+            df[i]=df[i].astype(str)
+            gplist.append(i)
+
+
+
+    
+    # grouped.iloc[1:]=df.groupby(gplist)[sumcoloumn].sum().reset_index()
+    grouped=df.groupby(gplist)[sumcoloumn].sum().reset_index()
+    
+
+
+    grouped.columns=oldcol
+    excel_book=openpyxl.Workbook()
+    excel_out=excel_book.active
+    
+    
+    for row in dataframe_to_rows(grouped,index=False,header=True):
+        excel_out.append(row)
+        
+        
+    
+    excel_book.save(output_file)
---
+++ myapp.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,send_from_directory,request,send_file
+from group import process_excel
+
+import pandas as pd
+
+
+
+app=Flask(__name__)
+@app.route("/downloads/<path:filename>")
+def allow_download(filename):
+    return send_file("downloads/"+filename,as_attachment=True)
+
+@app.route('/', methods=["GET","POST"])
+
+
+def open_index():
+    if request.method=="POST":
+        if 'file' not in request.files:
+            return render_template('index.html',message="no file selected")
+        excelfile=request.files['file']
+        if excelfile.filename=="":
+
+            return render_template('index.html',message="chuitya select file")
+        if excelfile.filename.rsplit(".")[-1]!="xlsx":
+            return render_template('index.html',message="please select excel file only")
+        if request.form["coloumnnumber"]=="":
+            return render_template('index.html',message="please put column number")
+        try:
+            input_file_pate="uploads/"+excelfile.filename
+            output_file_pate="downloads/"+excelfile.filename
+            sumcoloumn=int(request.form['coloumnnumber'])
+            excelfile.save(input_file_pate)
+            process_excel(input_file_pate,output_file_pate,sumcoloumn)
+            return render_template("index.html",message="file process succesfully",dowload_file=output_file_pate)
+
+        except Exception as e:
+            return render_template("index.html",message=e)    
+
+
+    return render_template("index.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
